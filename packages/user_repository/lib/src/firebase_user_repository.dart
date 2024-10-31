8fb28b6e636b7c0f6cd3c8d014931f467f523325
kelemnati <natikeleme1@gmail.com>
2024-10-31 00:36:37 -0700

    Completed authentication functionality

--- packages/user_repository/lib/src/firebase_user_repository.dart
+++ packages/user_repository/lib/src/firebase_user_repository.dart
@@ -1,5 +1,59 @@
 //a class that handles all the methods with firebase like sign in, sign up, and it implements user_repo.dart file
 
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:user_repository/src/models/my_user.dart';
 import 'package:user_repository/src/user_repo.dart';
 
-class FirebaseUserRepository implements UserRepository {}
+class FirebaseUserRepository implements UserRepository {
+  final FirebaseAuth _firebaseAuth;
+
+  FirebaseUserRepository({
+    FirebaseAuth? firebaseAuth,
+  }) : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+
+  @override
+  Future<MyUser> signUp(MyUser myUser, String password) async {
+    try {
+      UserCredential user = await _firebaseAuth.createUserWithEmailAndPassword(
+          email: myUser.email, password: password);
+      myUser = myUser.copyWith(id: user.user!.uid);
+      return myUser;
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> signIn(String emial, String password) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+          email: emial, password: password);
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> logOut() async {
+    try {
+      await _firebaseAuth.signOut();
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> resetPassword(String email) async {
+    try {
+      await _firebaseAuth.sendPasswordResetEmail(email: email);
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+}
--- packages/user_repository/lib/src/user_repo.dart
+++ packages/user_repository/lib/src/user_repo.dart
@@ -1,3 +1,19 @@
 // all the abstract class that will be implemented on the firebase_user_repository
 
-abstract class UserRepository {}
+import 'dart:ffi';
+
+import 'package:user_repository/src/models/models.dart';
+
+abstract class UserRepository {
+  // for sign in we use email and password
+  Future<void> signIn(String emial, String password);
+
+  // for logout
+  Future<void> logOut();
+
+  // for sign up we use MyUser class
+  Future<MyUser> signUp(MyUser myUser, String password);
+
+  // for reset password
+  Future<void> resetPassword(String email);
+}
